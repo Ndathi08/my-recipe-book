13e08856eac4a733f8db07107e1cdd0acbe7eff4
Ndathi08 <hellen.ndathi@strathmore.edu>
2024-04-14 20:41:57 +0300

    feat: edit navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,9 +13,7 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import HomeIcon from "@mui/icons-material/Home";
 import InfoIcon from "@mui/icons-material/Info";
-import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
-import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
-import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
